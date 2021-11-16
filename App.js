1f5b7c33d1c3ca8647337e584578be9b01305757
PhucTruong98 <phucthanhtruong0405@gmail.com>
2021-11-15 18:21:17 -0800

    testing out the pull request

--- App.js
+++ App.js
@@ -10,7 +10,7 @@ export default function App() {
         alignItems: "center",
       }}
     >
-      <Text>Universal React ugh ghg ghgh with Expo</Text>
+      <Text>Universal React ugh ghg new code ghgh with Expo</Text>
     </View>
   );
 }

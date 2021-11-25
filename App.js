828d3017640e273a840d9c181f6e08c355b0834b
esilva2 <esilva1246@gmail.com>
2021-11-24 17:21:44 -0800

    second commit

--- App.js
+++ App.js
@@ -1,16 +1,59 @@
 import * as React from "react";
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet ,Button,TextInput,TouchableWithoutFeedback, Keyboard} from "react-native";
+
+const Dissmiss= ({children})=>(
+  <TouchableWithoutFeedback onPress={()=> Keyboard.dismiss()}>
+    {children}
+  </TouchableWithoutFeedback>
+);
 
 export default function App() {
   return (
+  <Dissmiss>
     <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Emmanuel Silva</Text>
-    </View>
+    style={{
+      marginBottom: 30,
+      justifyContent: "center",
+      alignItems: "center",
+        }}
+      >
+        <View style={{ alignSelf:'flex-start', paddingTop:70, paddingBottom: 70, marginRight:50,justifyContent:'center', alignItems:'center', width: '100%',backgroundColor:'#4970FA'}}>
+
+            <View style={{alighItems: 'center',justifyContent: 'center', width:'20%', backgroundColor:'white' }} border-style='dotted'>
+              <Text style={styles.logo}>Logo</Text>
+            </View> 
+            
+        </View >
+
+          <TextInput multiline={true} placeholder="Email"/>
+          <TextInput multiline={true} placeholder="SignUp"/>
+      
+          <Button  style={s.btn} title="Login"/>
+          <Button  style={s.btn} title ="SignUp"/>
+      
+        
+      </View>
+
+  </Dissmiss>
+
+
   );
 }
+
+const s = StyleSheet.create({
+    btn:{
+      padding: 40,
+      borderRadius:80,
+    }
+});
+
+const styles = StyleSheet.create({
+  logo:{
+    height:50,
+    justifyContent:'center',
+    textAlign: 'center',
+    alignItems:'center',
+    fontSize: 20,
+    color: 'black'
+  }
+});

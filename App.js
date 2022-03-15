733ceaed3ebf4c43b6b85c43df85528c3f9720bb
Jerald Velicaria <69361447+jcveli@users.noreply.github.com>
2022-03-14 23:22:42 -0700

    #60,113 fixed Role selection and added checkbox agreement

--- App.js
+++ App.js
@@ -17,16 +17,14 @@ export default function App() {
   return (
     <NavigationContainer>{
       <Stack.Navigator>
-
-        <Stack.Screen name="CourseEdit" component={CourseEditScreen}   options={{ title: 'Course Edit' }}/>
- 
+        
+        
         <Stack.Screen name="LogIn" component={LogInScreen}   options={{ title: 'LogIn' }}/>
         <Stack.Screen name="SignUp" component={SignUpScreen}   options={{ title: 'Sign Up' }}/>
         <Stack.Screen name="ClassSearchScreen" component={ClassSearchScreen} options={{title: 'ClassSearchScreen', headerShown: false}}/>
-
         <Stack.Screen name="dashboard" component={Dash}/>
         <Stack.Screen name="Profile" component={Profile}   options={{ title: 'Profile' }}/>
-
+        <Stack.Screen name="CourseEdit" component={CourseEditScreen}   options={{ title: 'Course Edit' }}/>
 
       </Stack.Navigator>
       
